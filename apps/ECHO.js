e9a9515259ba0b353df576b27fe78edeb3578ed9
zolay <moe@zozo.me>
2024-07-01 15:48:49 +0800

    feat: add ECHO.js

---
+++ apps/ECHO.js
@@ -0,0 +1,31 @@
+import {plugin} from '#plugin'
+
+const echoReg = /#ECHO (.+)/i
+
+/**
+ * ECHO
+ */
+export default class ECHO extends plugin {
+
+  constructor() {
+    super({
+      name: "ECHO",
+      dsc: "ECHO回音",
+      event: "message",
+      rule: [
+        {
+          reg: echoReg,
+          fnc: 'echo',
+          permission: 'master',
+        }
+      ]
+    })
+  }
+
+  async echo() {
+    const {msg} = this.e
+    const match = msg.match(echoReg)
+    return this.reply(match[1])
+  }
+
+}
